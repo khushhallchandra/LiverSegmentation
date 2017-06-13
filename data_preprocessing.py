0f0a6597e6897c13eed953ebd73820e98ac00493
Roni Gurvich <roni752@gmail.com>
2017-06-13 20:03:49 +0300

    refactoring

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,4 +1,3 @@
-"""all functions necessary for preprocessing the dataset before training """
 import os
 import numpy as np
 from scipy.misc import imread
@@ -8,7 +7,22 @@ from tqdm import tqdm
 from utils import to_categorical
 
 
-def load_data(data_folder, seg_folder, im_size=(128, 128, 3), n_classes=2, ext='png'):
+def preprocess_data_folder():
+    data_folder = os.path.join('.', 'data', 'ct')
+    label_folder = os.path.join('.', 'data', 'seg')
+
+    dataset = load_data(data_folder, label_folder,
+                        im_size=(512, 512, 1), n_classes=3)
+
+    dataset['x'], dataset['xv'], dataset['m'], dataset['s'] = \
+        normalize_data(dataset['x'], dataset['xv'])
+
+    dataset_file = os.path.join('.', 'data', 'liver_dataset.npz')
+    np.savez(dataset_file, **dataset)
+
+
+def load_data(data_folder, seg_folder,
+              im_size=(128, 128, 3), n_classes=2, ext='png'):
     """loads images into numpy arrays"""
     train_list = glob(os.path.join(data_folder, 'train', '*.' + ext))
     val_list = glob(os.path.join(data_folder, 'val', '*.' + ext))
@@ -17,17 +31,16 @@ def load_data(data_folder, seg_folder, im_size=(128, 128, 3), n_classes=2, ext='
     n_val = len(val_list)
 
     # init numpy arrays
-    x = np.zeros((n_train, im_size[2], im_size[0], im_size[1]))
-    y = np.zeros((n_train, n_classes, im_size[0], im_size[1]))
-    xv = np.zeros((n_val, im_size[2], im_size[0], im_size[1]))
-    yv = np.zeros((n_val, n_classes, im_size[0], im_size[1]))
+    dataset = init_dataset(im_size, n_train, n_val, n_classes)
 
-    for train_i, imname in enumerate(train_list):
-        bname = os.path.basename(imname)
-        labelname = os.path.join(seg_folder, 'train', bname.replace('ct', 'seg'))
+    for train_i, im_name in enumerate(train_list):
+        bname = os.path.basename(im_name)
+        labelname = os.path.join(
+            seg_folder, 'train', bname.replace('ct', 'seg'))
 
-        im = imread(imname)
-        cat_label = to_categorical(imread(labelname)[:, :, 0] / 127, n_classes)
+        im = imread(im_name)
+        scaled_label = imread(labelname)[:, :, 0] / 127
+        cat_label = to_categorical(scaled_label, n_classes)
 
         if np.ndim(im) < len(im_size):
             im = np.expand_dims(im, 2)
@@ -38,21 +51,21 @@ def load_data(data_folder, seg_folder, im_size=(128, 128, 3), n_classes=2, ext='
         im = np.rollaxis(im, 2)
         cat_label = np.rollaxis(cat_label, 2)
 
-        x[train_i] = im
-        y[train_i] = cat_label
+        dataset['x'][train_i] = im
+        dataset['y'][train_i] = cat_label
 
         # sanity check
         # plt.figure(1)
         # plt.clf()
         # plt.imshow(im[:,:,0], cmap='gray')
-        # plt.imshow(label[:,:,0], cmap='jet', alpha=0.3, vmin=0, vmax=2)
+        # plt.imshow(scaled_label[:,:,0], cmap='jet', alpha=0.3, vmin=0, vmax=2)
         # plt.pause(0.2)
 
-    for val_i, imname in enumerate(val_list):
-        bname = os.path.basename(imname)
+    for val_i, im_name in enumerate(val_list):
+        bname = os.path.basename(im_name)
         labelname = os.path.join(seg_folder, 'val', bname.replace('ct', 'seg'))
 
-        im = imread(imname)
+        im = imread(im_name)
         cat_label = to_categorical(imread(labelname)[:, :, 0] / 127, n_classes)
 
         if np.ndim(im) < len(im_size):
@@ -64,16 +77,25 @@ def load_data(data_folder, seg_folder, im_size=(128, 128, 3), n_classes=2, ext='
         im = np.rollaxis(im, 2)
         cat_label = np.rollaxis(cat_label, 2)
 
-        xv[val_i] = im
-        yv[val_i] = cat_label
-    return x, y, xv, yv
+        dataset['xv'][val_i] = im
+        dataset['yv'][val_i] = cat_label
+    return dataset
+
+
+def init_dataset(im_size, n_train, n_val, n_classes):
+    x = np.zeros((n_train, im_size[2], im_size[0], im_size[1]))
+    y = np.zeros((n_train, n_classes, im_size[0], im_size[1]))
+    xv = np.zeros((n_val, im_size[2], im_size[0], im_size[1]))
+    yv = np.zeros((n_val, n_classes, im_size[0], im_size[1]))
+    dataset = dict(x=x, y=y, xv=xv, yv=yv)
+    return dataset
 
 
 def normalize_data(x, xv):
-    """for each sample in dataset subtracts pixelwise mean, and divides by pixelwise std"""
+    """for each sample in dataset subtracts pixelwise mean,
+    and divides by pixelwise std"""
     m = np.zeros((x.shape[1], x.shape[2], x.shape[3]))
     s2 = np.zeros((x.shape[1], x.shape[2], x.shape[3]))
-    s = np.zeros((x.shape[1], x.shape[2], x.shape[3]))
 
     n_train = x.shape[0]
     n_val = xv.shape[0]
@@ -94,22 +116,10 @@ def normalize_data(x, xv):
     s = np.sqrt(s2)
     s += 1  # to avoid division by zero
 
-    # noralize data
+    # normalize data
     for train_i in tqdm(range(x.shape[0]), desc='normalizing training data'):
         x[train_i] = (x[train_i] - m) / s
     for val_i in tqdm(range(xv.shape[0]), desc='normalizing val data'):
         xv[val_i] = (xv[val_i] - m) / s
 
     return x, xv, m, s
-
-
-def run_default():
-    # import matplotlib.pyplot as plt
-
-    data_folder = os.path.join('.', 'data', 'ct')
-    label_folder = os.path.join('.', 'data', 'seg')
-
-    x, y, xv, yv = load_data(data_folder, label_folder, im_size=(512, 512, 1), n_classes=3)
-    x, xv, m, s = normalize_data(x, xv)
-
-    np.savez(os.path.join('.', 'data', 'liver_dataset.npz'), x=x, y=y, xv=xv, yv=yv, m=m, s=s)
